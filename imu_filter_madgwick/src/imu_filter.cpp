2d70e4433b914bb871863469cae8d670d38776f8
Martin Günther <martin.guenther@dfki.de>
2015-06-24 15:29:35 +0200

    add deprecation message for use_magnetic_field_msg

--- imu_filter_madgwick/src/imu_filter.cpp
+++ imu_filter_madgwick/src/imu_filter.cpp
@@ -52,7 +52,11 @@ ImuFilter::ImuFilter(ros::NodeHandle nh, ros::NodeHandle nh_private):
 
   // For ROS Jade, make this default to true.
   if (!nh_private_.getParam ("use_magnetic_field_msg", use_magnetic_field_msg_))
-    use_magnetic_field_msg_ = false;
+  {
+      ROS_WARN("Deprecation Warning: The parameter use_magnetic_field_msg was not set, default is 'false'.");
+      ROS_WARN("Starting with ROS Jade, use_magnetic_field_msg will default to 'true'!");
+      use_magnetic_field_msg_ = false;
+  }
 
   // check for illegal constant_dt values
   if (constant_dt_ < 0.0)
