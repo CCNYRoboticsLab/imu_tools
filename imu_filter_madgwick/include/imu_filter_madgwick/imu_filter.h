75749e2746699000fd74651c4011c0d2ec41e5a0
Michael Stoll <michael.stoll@stolltec.com>
2015-07-29 08:35:58 +0200

    Splitted ImuFilter class into ImuFilter and ImuFilterRos

--- imu_filter_madgwick/include/imu_filter_madgwick/imu_filter.h
+++ imu_filter_madgwick/include/imu_filter_madgwick/imu_filter.h
@@ -25,113 +25,24 @@
 #ifndef IMU_FILTER_MADWICK_IMU_FILTER_H
 #define IMU_FILTER_MADWICK_IMU_FILTER_H
 
-#include <math.h>
-
-#include <ros/ros.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/MagneticField.h>
-#include <geometry_msgs/Vector3Stamped.h>
-#include "tf2_ros/transform_broadcaster.h"
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <dynamic_reconfigure/server.h>
-
-#include "imu_filter_madgwick/ImuFilterMadgwickConfig.h"
-
 class ImuFilter
 {
-  typedef sensor_msgs::Imu              ImuMsg;
-  typedef sensor_msgs::MagneticField    MagMsg;
-  typedef geometry_msgs::Vector3Stamped MagVectorMsg;
-
-  typedef message_filters::sync_policies::ApproximateTime<ImuMsg, MagMsg> SyncPolicy;
-  typedef message_filters::Synchronizer<SyncPolicy> Synchronizer;
-  typedef message_filters::Subscriber<ImuMsg> ImuSubscriber;
-  typedef message_filters::Subscriber<MagMsg> MagSubscriber;
-  typedef message_filters::Subscriber<MagVectorMsg> MagVectorSubscriber;
-
-  typedef imu_filter_madgwick::ImuFilterMadgwickConfig   FilterConfig;
-  typedef dynamic_reconfigure::Server<FilterConfig>   FilterConfigServer;
-
   public:
 
-    ImuFilter(ros::NodeHandle nh, ros::NodeHandle nh_private);
+    ImuFilter();
     virtual ~ImuFilter();
 
   private:
-
-    // **** ROS-related
-
-    ros::NodeHandle nh_;
-    ros::NodeHandle nh_private_;
-    
-    boost::shared_ptr<Synchronizer> sync_;
-    boost::shared_ptr<ImuSubscriber> imu_subscriber_;
-    boost::shared_ptr<MagSubscriber> mag_subscriber_;
-
-    // Adapter to support the use_magnetic_field_msg param.
-    boost::shared_ptr<MagVectorSubscriber> vector_mag_subscriber_;
-    ros::Publisher mag_republisher_;
-
-    ros::Publisher rpy_filtered_debug_publisher_;
-    ros::Publisher rpy_raw_debug_publisher_;
-    ros::Publisher imu_publisher_;
-    tf2_ros::TransformBroadcaster tf_broadcaster_;
-
-    boost::shared_ptr<FilterConfigServer> config_server_;
-    
     // **** paramaters
-
     double gain_;     // algorithm gain
     double zeta_;	  // gyro drift bias gain
-    bool use_mag_;
-    bool use_magnetic_field_msg_;
-    bool publish_tf_;
-    bool reverse_tf_;
-    std::string fixed_frame_;
-    std::string imu_frame_;
-    double constant_dt_;
-    bool publish_debug_topics_;
-    geometry_msgs::Vector3 mag_bias_;
-    double orientation_variance_;
 
     // **** state variables
-    boost::mutex mutex_;
-    bool initialized_;
     double q0, q1, q2, q3;  // quaternion
-    ros::Time last_time_;
     float w_bx_, w_by_, w_bz_; // 
 
     // **** member functions
 
-    void imuMagCallback(const ImuMsg::ConstPtr& imu_msg_raw,
-                        const MagMsg::ConstPtr& mav_msg);
-
-    void imuCallback(const ImuMsg::ConstPtr& imu_msg_raw);
-
-    void imuMagVectorCallback(const MagVectorMsg::ConstPtr& mag_vector_msg);
-
-    void publishFilteredMsg(const ImuMsg::ConstPtr& imu_msg_raw);
-    void publishTransform(const ImuMsg::ConstPtr& imu_msg_raw);
-
-    void publishRawMsg(const ros::Time& t,
-                       float roll, float pitch, float yaw);
-
-    void madgwickAHRSupdate(float gx, float gy, float gz, 
-                            float ax, float ay, float az, 
-                            float mx, float my, float mz,
-                            float dt);
-
-    void madgwickAHRSupdateIMU(float gx, float gy, float gz, 
-                               float ax, float ay, float az,
-                               float dt);
-
-    void reconfigCallback(FilterConfig& config, uint32_t level);
-
-    void computeRPY(float ax, float ay, float az, 
-                    float mx, float my, float mz,
-                    float& roll, float& pitch, float& yaw);
     // Fast inverse square-root
     // See: http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Reciprocal_of_the_square_root
     static float invSqrt(float x)
@@ -148,6 +59,46 @@ class ImuFilter
       u.x = u.x * (1.5f - xhalf * u.x * u.x);
       return u.x;
     }
+
+public:
+    void setAlgorithmGain(double gain)
+    {
+        gain_ = gain;
+    }
+
+    void setDriftBiasGain(double zeta)
+    {
+        zeta_ = zeta;
+    }
+
+    void getOrientation(double& q0, double& q1, double& q2, double& q3)
+    {
+        q0 = this->q0;
+        q1 = this->q1;
+        q2 = this->q2;
+        q3 = this->q3;
+    }
+
+    void setOrientation(double q0, double q1, double q2, double q3)
+    {
+        this->q0 = q0;
+        this->q1 = q1;
+        this->q2 = q2;
+        this->q3 = q3;
+
+        w_bx_ = 0;
+        w_by_ = 0;
+        w_bz_ = 0;
+    }
+
+    void madgwickAHRSupdate(float gx, float gy, float gz,
+                            float ax, float ay, float az,
+                            float mx, float my, float mz,
+                            float dt);
+
+    void madgwickAHRSupdateIMU(float gx, float gy, float gz,
+                               float ax, float ay, float az,
+                               float dt);
 };
 
 #endif // IMU_FILTER_IMU_MADWICK_FILTER_H
