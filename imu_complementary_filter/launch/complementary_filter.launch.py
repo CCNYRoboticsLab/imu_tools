2d647aa89cab9d6b9e63178ec2decec539518cfe
Maximilian Schik <max.schik@googlemail.com>
2021-02-11 17:41:00 +0100

    adds python launch file

---
+++ imu_complementary_filter/launch/complementary_filter.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    
+    return LaunchDescription([
+        Node(
+            package='imu_complementary_filter',
+            node_executable='complementary_filter_node',
+            name='complementary_filter_gain_node',
+            output='screen',
+            parameters=[
+                {'do_bias_estimation': True},
+                {'do_adaptive_gain': True},
+                {'use_mag': False},
+                {'gain_acc': 0.01},
+                {'gain_mag': 0.01},
+                ]
+            )
+        ])
+
